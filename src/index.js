4ab13980adf91291cf552cfae5460dc4a6caa3a0
ninujob <ninujob15@gmail.com>
2022-11-29 11:29:10 +0530

    deployed the app and start using hash-router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 // eslint-disable-next-line
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -12,9 +12,9 @@ import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want your app to work offline and load faster, you can change
