b8589c77e9c1c03808c3e0eefda8461b44749059
ninujob <ninujob15@gmail.com>
2022-11-29 11:02:54 +0530

    Updates

---
+++ precache-manifest.9aa350865fa5ca20bb43c8263c16d7e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb257c96caa5316c9c50f807ebad75b8",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "9bd516f801868ca563a8",
+    "url": "/box-office/static/css/main.3cd6ee85.chunk.css"
+  },
+  {
+    "revision": "7412fb3e8307a7d987a7",
+    "url": "/box-office/static/js/2.16001ff7.chunk.js"
+  },
+  {
+    "revision": "1014b76b1932b9c4caafc220184395a7",
+    "url": "/box-office/static/js/2.16001ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd516f801868ca563a8",
+    "url": "/box-office/static/js/main.0628a348.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/box-office/static/js/main.0628a348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29ffa563950024eada9a",
+    "url": "/box-office/static/js/runtime-main.a4072daa.js"
+  }
+]);
